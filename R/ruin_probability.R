276b0bbfcf02bc63a1df50ba5b93db2d6795a25a
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2018-07-11 15:57:34 +0200

    Replace pbapply package by parallel. pbapply does not support required argument mc.set.seed (throws different seeds to cores). .onLoad change to stable L'Ecuyer-CMRG RNG to have "independence" among cores. Parallelalize ruin_probablility.R

--- R/ruin_probability.R
+++ R/ruin_probability.R
@@ -1,19 +1,58 @@
 #' @export
 ruin_probability <- function(model,
                              time_horizon,
-                             simulation_number = 10000) { # ,
-                             # cluster = NULL) {
+                             simulation_number = 10000,
+                             parallel = TRUE) {
 
-    browser()
+    if(parallel) {
 
-    processes <- pbapply::pbreplicate(
-        n = simulation_number,
-        expr = do.call(simulate_path,
-                       list(model = model,
-                            max_time_horizon = time_horizon)),
-        simplify = FALSE# ,
-        # cl = cluster
-    )
+        if(.Platform[["OS.type"]] == "windows") {
+
+            # write a code
+
+        } else if(.Platform[["OS.type"]] == "unix") {
+
+            browser()
+
+            ncores <- parallel::detectCores()
+
+            processes <- parallel::mclapply(
+                rep(time_horizon, simulation_number),
+                function(x) simulate_path(
+                    model = model,
+                    max_time_horizon = x
+                ),
+                mc.set.seed = TRUE,
+                mc.cores = ncores
+            )
+
+        }
+
+
+    } else {
+
+        processes <- replicate(
+            n = simulation_number,
+            expr = do.call(simulate_path,
+                           list(model = model,
+                                max_time_horizon = time_horizon)),
+            simplify = FALSE
+        )
+
+    }
+
+
+
+
+
+    # processes <- pbapply::pbreplicate(
+    #     n = simulation_number,
+    #     expr = do.call(simulate_path,
+    #                    list(model = model,
+    #                         max_time_horizon = time_horizon)),
+    #     simplify = FALSE# ,
+    #     # cl = cluster
+    # )
 
     ruined <- sapply(processes, function(prc) prc@is_ruined)
 
