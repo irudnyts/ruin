16fe39baca9a7dace7a84a1ad6733b17afb9572e
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2018-07-11 17:30:44 +0200

    Refine ruin_probability function: retunrs probability with CI, and the list of simulated processes. Write parallelization for Windows (unstable). Add comments.

--- R/ruin_probability.R
+++ R/ruin_probability.R
@@ -1,24 +1,54 @@
 #' @export
 ruin_probability <- function(model,
                              time_horizon,
-                             simulation_number = 10000,
+                             simulation_number = NULL,
+                             ci_level = NULL,
                              parallel = TRUE) {
+
+    # set default arguments
+    #---------------------------------------------------------------------------
+
+    if(is.null(simulation_number))
+        simulation_number <- 10000
+
+    if(is.null(ci_level))
+        ci_level <- 0.95
+
+    # validate arguments
+    #---------------------------------------------------------------------------
+
+    # simulate
+    #---------------------------------------------------------------------------
 
     if(parallel) {
 
+        # detect the number of cores
+        ncores <- parallel::detectCores()
+
+        # parallelize for Windows
         if(.Platform[["OS.type"]] == "windows") {
 
-            # write a code
+            # XXX
 
+            cluster <- parallel::makePSOCKcluster(ncores)
+
+            parallel::clusterSetRNGStream(cl = cluster)
+
+            processes <- parallel::parLapply(
+                cl = cluster,
+                X = rep(time_horizon, simulation_number),
+                fun = function(x) simulate_path(
+                    model = model,
+                    max_time_horizon = x
+                )
+            )
+
+        # parallelize for Unix
         } else if(.Platform[["OS.type"]] == "unix") {
 
-            browser()
-
-            ncores <- parallel::detectCores()
-
             processes <- parallel::mclapply(
-                rep(time_horizon, simulation_number),
-                function(x) simulate_path(
+                X = rep(time_horizon, simulation_number),
+                FUN = function(x) simulate_path(
                     model = model,
                     max_time_horizon = x
                 ),
@@ -41,32 +71,23 @@ ruin_probability <- function(model,
 
     }
 
-
-
-
-
-    # processes <- pbapply::pbreplicate(
-    #     n = simulation_number,
-    #     expr = do.call(simulate_path,
-    #                    list(model = model,
-    #                         max_time_horizon = time_horizon)),
-    #     simplify = FALSE# ,
-    #     # cl = cluster
-    # )
-
+    # define which processes have been ruined
     ruined <- sapply(processes, function(prc) prc@is_ruined)
 
-    # p <- mean(ruined)
-    # std <- sd(ruined)
-    #
-    # z <- qnorm(0.5 + ci_level / 2)
-    #
-    # list(
-    #     lower_bound = p - z * std / sqrt(simulation_number),
-    #     estimate = p,
-    #     upper_bound = p + z * std / sqrt(simulation_number)
-    # )
+    p <- mean(ruined)
+    std <- sd(ruined)
 
-    mean(ruined)
+    z <- qnorm(0.5 + ci_level / 2)
+
+    rval <- list(
+        ruin_probability = c(
+            lower_bound = p - z * std / sqrt(simulation_number),
+            estimate = p,
+            upper_bound = p + z * std / sqrt(simulation_number)
+        ),
+        simulated_processes = processes
+    )
+
+    return(rval)
 
 }
