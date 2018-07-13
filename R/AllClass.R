85f2ed47ada7db391944dce0f2d8d79d9d5f0e94
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2018-07-13 14:31:34 +0200

    Add simulate_path of CramerLundberg.

--- R/AllClass.R
+++ R/AllClass.R
@@ -1,57 +1,61 @@
-setClass(Class = "CramerLundberg",
-         slots = list(
-           initial_capital = "numeric",
-           premium_rate = "numeric",
-           claim_poisson_arrival_rate = "numeric",
-           claim_size_generator = "numeric",
-           claim_size_parameters = "numeric"
-         )
+setClass(
+  Class = "CramerLundberg",
+  slots = list(
+    initial_capital = "numeric",
+    premium_rate = "numeric",
+    claim_poisson_arrival_rate = "numeric",
+    claim_size_generator = "function",
+    claim_size_parameters = "list"
+  )
 )
 
-setClass(Class = "PathCramerLundberg",
-         slots = list(
-           model = "CramerLundberg",
-           path = "matrix",
-           claim_sizes = "numeric",
-           claim_arrival_times = "numeric",
-           time_horizon = "numeric",
-           is_ruined = "logical",
-           elapsed_time = "numeric",
-           max_time_horizon = "numeric",
-           max_simulation_time = "numeric",
-           seed = "integer"
-         )
+setClass(
+  Class = "PathCramerLundberg",
+  slots = list(
+    model = "CramerLundberg",
+    path = "matrix",
+    claim_sizes = "numeric",
+    claim_arrival_times = "numeric",
+    time_horizon = "numeric",
+    is_ruined = "logical",
+    elapsed_time = "numeric",
+    max_time_horizon = "numeric",
+    max_simulation_time = "numeric",
+    seed = "integer"
+  )
 )
 
-setClass(Class = "CramerLundbergCapitalInjections",
-         slots = list(
-           initial_capital = "numeric",
-           premium_rate = "numeric",
-           claim_poisson_arrival_rate = "numeric",
-           claim_size_mixing_parameter = "numeric",
-           claim_size_light_tail_generator = "function",
-           claim_size_light_tail_parameters = "list",
-           claim_size_heavy_tail_generator = "function",
-           claim_size_heavy_tail_parameters = "list",
-           capital_injection_poisson_rate = "numeric",
-           capital_injection_generator = "function",
-           capital_injection_parameters = "list"
-         )
+setClass(
+  Class = "CramerLundbergCapitalInjections",
+  slots = list(
+    initial_capital = "numeric",
+    premium_rate = "numeric",
+    claim_poisson_arrival_rate = "numeric",
+    claim_size_mixing_parameter = "numeric",
+    claim_size_light_tail_generator = "function",
+    claim_size_light_tail_parameters = "list",
+    claim_size_heavy_tail_generator = "function",
+    claim_size_heavy_tail_parameters = "list",
+    capital_injection_poisson_rate = "numeric",
+    capital_injection_generator = "function",
+    capital_injection_parameters = "list"
+  )
 )
 
-setClass(Class = "PathCramerLundbergCapitalInjections",
-         slots = list(
-           model = "CramerLundbergCapitalInjections",
-           path = "matrix",
-           claim_sizes = "numeric",
-           claim_arrival_times = "numeric",
-           capital_injection_sizes = "numeric",
-           capital_injection_arrival_times = "numeric",
-           time_horizon = "numeric",
-           is_ruined = "logical",
-           elapsed_time = "numeric",
-           max_time_horizon = "numeric",
-           max_simulation_time = "numeric",
-           seed = "integer"
-         )
+setClass(
+  Class = "PathCramerLundbergCapitalInjections",
+  slots = list(
+    model = "CramerLundbergCapitalInjections",
+    path = "matrix",
+    claim_sizes = "numeric",
+    claim_arrival_times = "numeric",
+    capital_injection_sizes = "numeric",
+    capital_injection_arrival_times = "numeric",
+    time_horizon = "numeric",
+    is_ruined = "logical",
+    elapsed_time = "numeric",
+    max_time_horizon = "numeric",
+    max_simulation_time = "numeric",
+    seed = "integer"
+  )
 )
--- R/methods-CramerLundberg.R
+++ R/methods-CramerLundberg.R
@@ -59,14 +59,8 @@ CramerLundberg <- function(initial_capital = NULL,
     initial_capital = initial_capital,
     premium_rate = premium_rate,
     claim_poisson_arrival_rate = claim_poisson_arrival_rate,
-    claim_size_mixing_parameter = claim_size_mixing_parameter,
-    claim_size_light_tail_generator = claim_size_light_tail_generator,
-    claim_size_light_tail_parameters = claim_size_light_tail_parameters,
-    claim_size_heavy_tail_generator = claim_size_heavy_tail_generator,
-    claim_size_heavy_tail_parameters = claim_size_heavy_tail_parameters,
-    capital_injection_poisson_rate = capital_injection_poisson_rate,
-    capital_injection_generator = capital_injection_generator,
-    capital_injection_parameters = capital_injection_parameters
+    claim_size_generator = claim_size_generator,
+    claim_size_parameters = claim_size_parameters
   )
 
   return(model)
@@ -81,6 +75,162 @@ setMethod(
                         max_simulation_time = NULL,
                         seed = NULL) {
 
+    # set default arguments
+    #---------------------------------------------------------------------------
+
+    if(is.null(max_time_horizon))
+      max_time_horizon <- Inf
+
+    if(is.null(max_simulation_time))
+      max_simulation_time <- Inf
+
+    if(is.null(seed)) {
+      seed <- .Random.seed
+    } else {
+      # .Random.seed <<- seed
+      assign(x = ".Random.seed", value = seed, envir = .GlobalEnv)
+    }
+
+    # validate arguments
+    #---------------------------------------------------------------------------
+
+    stopifnot(
+
+      isS4(model),
+
+      is.numeric(max_time_horizon) &&
+        length(max_time_horizon) == 1 &&
+        isFALSE(is.na(max_time_horizon)) &&
+        max_time_horizon > 0,
+
+      is.numeric(max_simulation_time) &&
+        length(max_simulation_time) == 1 &&
+        isFALSE(is.na(max_simulation_time)) &&
+        max_simulation_time > 0,
+
+      is.numeric(seed)
+
+    )
+
+    if(is.infinite(max_time_horizon) && is.infinite(max_simulation_time))
+      warning(paste0("Setting both max_time_horizon and max_simulation_time",
+                     "to Inf might result in an infinite loop."))
+
+    # define aliases
+    #---------------------------------------------------------------------------
+
+    u <- model@initial_capital
+    pr <- model@premium_rate
+    lambda <- model@claim_poisson_arrival_rate
+    f <- model@claim_size_generator
+    param <- model@claim_size_parameters
+
+    # simulate process
+    #-----------------------------------------------------------------------
+
+    # add n = 1 to distribution's parameters in order to generate only
+    # one r.v.
+    param[["n"]] <- 1
+
+    # initialize process
+    path <- matrix(NA, nrow = 1, ncol = 2)
+    colnames(path) <- c("time", "X")
+    path[1, ] <- c(0, u)
+
+    # auxiliary function for adding jumps to a path
+    add_jump_to_path <- function(path, arrival, size) {
+
+      path <- rbind(
+        path,
+        c(arrival,
+          path[nrow(path), 2] + (arrival - path[nrow(path), 1]) * pr)
+      )
+
+      path <- rbind(
+        path,
+        c(arrival,
+          path[nrow(path), 2] + size)
+      )
+      path
+    }
+
+    s_neg <- numeric() # positive jumps' sizes
+
+    a_neg <- numeric() # arrival times of negative jumps
+
+    ca_neg <- rexp(1, lambda) # current arrival time of a negative jump
+
+    is_ruined <- FALSE
+
+    start_time <- Sys.time() # set a timer
+
+    repeat{
+
+      if(as.numeric(difftime(time1 = Sys.time(),
+                             time2 = start_time,
+                             units = "secs")) < max_simulation_time) {
+
+        if(ca_neg < max_time_horizon) {
+
+          # current negative jump's size
+          cs_neg <- do.call(f, param)
+
+          path <- add_jump_to_path(path, ca_neg, -cs_neg)
+
+          s_neg <- c(s_neg, cs_neg)
+          a_neg <- c(a_neg, ca_neg)
+
+          if(path[nrow(path), 2] < 0) {
+            is_ruined <- TRUE
+            break
+          }
+
+          ca_neg <- ca_neg + rexp(1, lambda)
+
+        } else {
+
+          # add max_time_horizon to a path
+          path <- rbind(
+            path,
+            c(max_time_horizon,
+              path[nrow(path), 2] + (max_time_horizon - path[nrow(path), 1]) * pr)
+          )
+
+          break
+
+        }
+
+      } else {
+
+        break
+
+      }
+
+    }
+
+    end_time <- Sys.time()
+
+    elapsed_time <- as.numeric(difftime(time1 = end_time,
+                                        time2 = start_time,
+                                        units = "secs"))
+
+    # generate a returning object
+    process <- new(
+      Class = "PathCramerLundberg",
+      model = model,
+      path = path,
+      claim_sizes = s_neg,
+      claim_arrival_times = a_neg,
+      time_horizon = path[nrow(path), 1],
+      is_ruined = is_ruined,
+      elapsed_time = elapsed_time,
+      max_time_horizon = max_time_horizon,
+      max_simulation_time = max_simulation_time,
+      seed = seed
+    )
+
+    return(process)
 
   }
+
 )
