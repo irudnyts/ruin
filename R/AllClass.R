a3c74540229f26dde20481945f2b9f6ba20ccda1
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2018-07-23 14:13:30 +0200

    Add documentation for CLCI, SA, and SACI classes.

--- R/AllClass.R
+++ R/AllClass.R
@@ -61,6 +61,50 @@ setClass(
 #-------------------------------------------------------------------------------
 # CramerLundbergCapitalInjections classes
 
+#' A formal S4 class CramerLundbergCapitalInjections
+#'
+#' A formal S4 class representation of Cramer-Lundberg's extenssion that
+#' includes capital injections.
+#'
+#' The model is defined as follows:
+#' \deqn{X_(t) = u + ct + \sum_{k=1}^{N^{(+)}(t)} Y^{(+)}_k -
+#' \sum_{i=1}^{N^{(-)}(t)} Y^{(-)}_i}
+#' where \eqn{u} is the initial capital (\code{initial_capital}), \eqn{c} is the
+#' premium rate (\code{premium_rate}), \eqn{N^{(+)}(t)} is the Poisson process
+#' of positive jumps (capital injections) with intencity \eqn{\lambda^{(+)}}
+#' (\code{capital_injection_poisson_rate}), \eqn{Y^{(+)}_k} are
+#' iid capital injections' sizes (\code{capital_injection_size_generator}
+#' and \code{capital_injection_size_parameters}), \eqn{N^{(-)}(t)} is the
+#' Poisson process of negative jumps (claims) with intencity \eqn{\lambda^{(-)}}
+#' (\code{claim_poisson_arrival_rate}), \eqn{Y^{(-)}_i} are iid claim sizes
+#' (\code{claim_size_generator} and \code{claim_size_parameters}).
+#'
+#' Objects of class can be created only by using the constructor
+#' \code{\link{CramerLundbergCapitalInjections}}.
+#'
+#' @slot initial_capital a length one numeric non-negative vector specifying an
+#' initial capital.
+#' @slot premium_rate a length one numeric non-negative vector specifying a
+#' premium rate.
+#' @slot claim_poisson_arrival_rate a length one numeric positive vector
+#' specifying the rate of the Poisson process of claims' arrivals.
+#' @slot claim_size_generator a function indicating the random generator of
+#' claims' sizes.
+#' @slot claim_size_parameters a named list containing parameters for the
+#' random generator of claims' sizes.
+#' @slot capital_injection_poisson_rate a length one numeric positive vector
+#' specifying the rate of the Poisson process of capital injections' arrivals.
+#' @slot capital_injection_size_generator a function indicating the random
+#' generator of capital injections' sizes.
+#' @slot capital_injection_size_parameters a named list containing parameters
+#' for the random generator of capital injections' sizes.
+#'
+#' @seealso \code{\link{CramerLundbergCapitalInjections}}
+#'
+#' @references
+#' Breuera L., Badescu A. L. \emph{A generalised Gerber Shiu measure for
+#' Markov-additive risk processes with phase-type claims and capital
+#' injections}. Scandinavian Actuarial Journal, 2014(2): 93-115, 2014.
 setClass(
   Class = "CramerLundbergCapitalInjections",
   contains = "CramerLundberg",
@@ -92,6 +136,43 @@ setClass(
 #-------------------------------------------------------------------------------
 # SparreAndersen classes
 
+#' A formal S4 class SparreAndersen
+#'
+#' A formal S4 class representation of classical Sparre Andersen model.
+#'
+#' The model is defined as follows:
+#' \deqn{X(t) = u + ct - \sum_{i=1}^{N(t)} Y_i,}
+#' where \eqn{u} is the initial capital (\code{initial_capital}), \eqn{c} is the
+#' premium rate (\code{premium_rate}), \eqn{N(t)} is the renewal process defined
+#' by distribution of interarrival times (\code{claim_interarrival_generator}
+#' and \code{claim_interarrival_parameters}), \eqn{Y_i} are iid claim sizes
+#' (\code{claim_size_generator} and \code{claim_size_parameters}).
+#'
+#' Objects of class can be created only by using the constructor
+#' \code{\link{SparreAndersen}}.
+#'
+#' @slot initial_capital a length one numeric non-negative vector specifying an
+#' initial capital.
+#' @slot premium_rate a length one numeric non-negative vector specifying a
+#' premium rate.
+#' @slot claim_interarrival_generator a function indicating the random
+#' generator of claims' interarrival times.
+#' @slot claim_interarrival_parameters a named list containing parameters for
+#' the random generator of claims' interarrival times.
+#' @slot claim_size_generator a function indicating the random generator of
+#' claims' sizes.
+#' @slot claim_size_parameters a named list containing parameters for the
+#' random generator of claims' sizes.
+#'
+#' @seealso \code{\link{SparreAndersen}}
+#'
+#' @references \itemize{
+#' \item Andersen, E. Sparre. \emph{On the collective theory of risk in case of
+#' contagion between claims}. Transactions of the XVth International Congress
+#' of Actuaries, 2(6), 1957.
+#' \item Thorin O. \emph{Some Comments on the Sparre Andersen Model in the Risk
+#' Theory}. ASTIN Bulletin: The Journal of the IAA, 8(1):104-125, 1974.
+#' }
 setClass(
   Class = "SparreAndersen",
   slots = list(
@@ -123,7 +204,57 @@ setClass(
 #-------------------------------------------------------------------------------
 # SparreAndersenCapitalInjections classes
 
-
+#' A formal S4 class SparreAndersenCapitalInjections
+#'
+#' A formal S4 class representation of Sparre Andersen's extenssion that
+#' includes capital injections.
+#'
+#' The model is defined as follows:
+#' \deqn{X_(t) = u + ct + \sum_{k=1}^{N^{(+)}(t)} Y^{(+)}_k -
+#' \sum_{i=1}^{N^{(-)}(t)} Y^{(-)}_i}
+#' where \eqn{u} is the initial capital (\code{initial_capital}), \eqn{c} is the
+#' premium rate (\code{premium_rate}), \eqn{N^{(+)}(t)} is the renewal process
+#' of positive jumps (capital injections) defined by distribution of
+#' interarrival times (\code{capital_injection_interarrival_generator} and
+#' \code{capital_injection_interarrival_parameters}), \eqn{Y^{(+)}_k} are iid
+#' capital injections' sizes (\code{capital_injection_size_generator}
+#' and \code{capital_injection_size_parameters}), \eqn{N^{(-)}(t)} is the
+#' renewal process of claims defined by distribution of interarrival times
+#' (\code{claim_interarrival_generator} and
+#' \code{claim_interarrival_parameters}), \eqn{Y^{(-)}_i} are iid claim sizes
+#' (\code{claim_size_generator} and \code{claim_size_parameters}).
+#'
+#' Objects of class can be created only by using the constructor
+#' \code{\link{SparreAndersenCapitalInjections}}.
+#'
+#' @slot initial_capital a length one numeric non-negative vector specifying an
+#' initial capital.
+#' @slot premium_rate a length one numeric non-negative vector specifying a
+#' premium rate.
+#' @slot claim_interarrival_generator a function indicating the random
+#' generator of claims' interarrival times.
+#' @slot claim_interarrival_parameters a named list containing parameters for
+#' the random generator of claims' interarrival times.
+#' @slot claim_size_generator a function indicating the random generator of
+#' claims' sizes.
+#' @slot claim_size_parameters a named list containing parameters for the
+#' random generator of claims' sizes.
+#' @slot capital_injection_interarrival_generator a function indicating
+#' the random generator of capital injections' interarrival times.
+#' @slot capital_injection_interarrival_parameters a named list containing
+#' parameters for the random generator of capital injections' interarrival
+#' times.
+#' @slot capital_injection_size_generator a function indicating the random
+#' generator of capital injections' sizes.
+#' @slot capital_injection_size_parameters a named list containing parameters
+#' for the random generator of capital injections' sizes.
+#'
+#' @seealso \code{\link{SparreAndersenCapitalInjections}}
+#'
+#' @references
+#' Breuera L., Badescu A. L. \emph{A generalised Gerber Shiu measure for
+#' Markov-additive risk processes with phase-type claims and capital
+#' injections}. Scandinavian Actuarial Journal, 2014(2): 93-115, 2014.
 setClass(
   Class = "SparreAndersenCapitalInjections",
   contains = "SparreAndersen",
